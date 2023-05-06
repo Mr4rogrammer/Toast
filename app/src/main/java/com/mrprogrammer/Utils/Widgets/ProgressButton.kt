46e44af77ea9cf1601ee59a2b60956f24d4caece
krish11022002 <krish11022002@gmail.com>
2023-05-06 18:57:18 +0530

    Krishna | Custom View background color

--- app/src/main/java/com/mrprogrammer/Utils/Widgets/ProgressButton.kt
+++ app/src/main/java/com/mrprogrammer/Utils/Widgets/ProgressButton.kt
@@ -7,15 +7,18 @@ import android.view.ViewGroup
 import android.widget.ProgressBar
 import android.widget.RelativeLayout
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import com.mrprogrammer.Utils.R
 
 class ProgressButton : RelativeLayout{
     private lateinit var rootView : RelativeLayout
     private lateinit var textView : TextView
     private lateinit var progressBar: ProgressBar
+    private lateinit var cardView: CardView
     private var loaderColor = 0
-    private var text = ""
-
+    private var text = "Mr.Programmer"
+    private var textColor = 0
+    private var backgroundColor = 0
 
     constructor(context:Context): super(context) {
         initView(context)
@@ -30,12 +33,12 @@ class ProgressButton : RelativeLayout{
     }
 
     private fun getDataFromAtts(context: Context,attrs: AttributeSet){
-      val data = context?.obtainStyledAttributes(attrs,R.styleable.ButtonWithLoader)
-        if(data != null){
-            text = data.getString(R.styleable.ButtonWithLoader_mrtext).toString()
-            loaderColor = data.getColor(R.styleable.ButtonWithLoader_mrloadercolor,context.resources.getColor(R.color.white))
-            data.recycle()
-        }
+        val data = context.obtainStyledAttributes(attrs,R.styleable.ButtonWithLoader)
+        text = data.getString(R.styleable.ButtonWithLoader_text).toString()
+        loaderColor = data.getColor(R.styleable.ButtonWithLoader_loaderColor,context.resources.getColor(R.color.white))
+        textColor = data.getColor(R.styleable.ButtonWithLoader_textColor,context.resources.getColor(R.color.black))
+        backgroundColor = data.getColor(R.styleable.ButtonWithLoader_backgroundColor,context.resources.getColor(R.color.error_left))
+        data.recycle()
 
     }
 
@@ -45,9 +48,12 @@ class ProgressButton : RelativeLayout{
         rootView = findViewById(R.id._mr_MainLayout)
         textView = findViewById(R.id._mr_text)
         progressBar = findViewById(R.id._mr_loader)
+        cardView = findViewById(R.id._mr_card)
         if(text.isNotEmpty()) {
             textView.text = text
         }
+        textView.setTextColor(textColor)
+        cardView.setCardBackgroundColor(backgroundColor)
         progressBar.indeterminateDrawable.setColorFilter(loaderColor,android.graphics.PorterDuff.Mode.MULTIPLY);
         refreshDrawableState()
     }
